b1822726aee4800a6a91374ab155aa0f20cf4353
Jay Hayes <jayhayes@Jays-MacBook-Pro.local>
2024-05-06 21:24:17 +0100

    1 hour commit, provides array data for use in form, creates all functionality inside CurrencyConverter and adds some styling to this to give it some shape. Not as pretty as the home page but couldn't do more in the hour

--- src/app/converter/page.tsx
+++ src/app/converter/page.tsx
@@ -6,8 +6,11 @@ export const Converter = async () => {
     
     const currenciesData = await getCurrencyData();
 
+    const currenciesList = currenciesData && Object.entries(currenciesData).map(([currency]) => currency)
+
     return (
-        <div>
+        <div className="flex min-h-screen flex-col items-center justify-between p-12">
+            {currenciesList && <CurrencyConverter currencies={currenciesList}/>}
         </div>
     );
 };
--- src/components/CurrencyConverter.tsx
+++ src/components/CurrencyConverter.tsx
@@ -1,17 +1,64 @@
 "use client";
 
+import { useState } from "react";
+
 import { ConvertedDetails } from "@/types/types";
 import { getConversionToBitcoin } from "@/api";
 
-
 interface Props {
   currencies: string[];
-};
+}
 
 export const CurrencyConverter = ({ currencies }: Props) => {
+  const [selectedCurrency, setSelectedCurrency] = useState<string>("");
+  const [conversionAmount, setConversionAmount] = useState<string>("");
+  const [convertedDetails, setConvertedDetails] = useState<ConvertedDetails>();
+
+  const handleSubmit = async () => {
+    const bitcoinConversionDetails = await getConversionToBitcoin(
+      selectedCurrency,
+      conversionAmount
+    );
+    setConvertedDetails(bitcoinConversionDetails);
+  };
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center">
+      <div className="mb-3">
+        <h1 className="md:text-4xl sm:text-2xl p-5">Currency Converter</h1>
+        <div className="p-3">
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Enter amount to convert..."
+            type="text"
+            value={conversionAmount}
+            onChange={(e) => setConversionAmount(e.target.value)}
+          />
+        </div>
+        <div className="p-3">
+          <select
+            className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            value={selectedCurrency}
+            onChange={(e) => setSelectedCurrency(e.target.value)}
+          >
+            <option value="">Select a currency...</option>
+            {currencies.map((currency) => (
+              <option value={currency} key={currency}>
+                {currency}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button
+          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+          onClick={handleSubmit}
+        >
+          Convert
+        </button>
+      </div>
+      {convertedDetails && (
+        <h2 className="md:text-2xl sm:text-1xl">{`${convertedDetails.currency}${convertedDetails.value} is equal to ${convertedDetails.bitcoinAmount} Bitcoin.`}</h2>
+      )}
     </div>
   );
 };
